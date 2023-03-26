b575de4100d7fb34febc2759481d1c29ddb49e5f
3700 <3700@generative.xyz>
2023-03-26 23:13:32 +0700

    add BNS example

---
+++ contracts/BNS.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BNS is ERC721 {
+
+        using Counters for Counters.Counter;
+        Counters.Counter private _tokenIds;
+
+        mapping(uint256 => bytes) public registry;
+        mapping(bytes => bool) public registered;
+
+        mapping(uint256 => address) public resolver;
+
+        constructor() ERC721("Bitcoin Name System", "BNS") {}
+
+        function register(address owner, bytes memory name) 
+                public 
+                returns (uint256) 
+        {
+                require(!registered[name]);
+
+                uint256 id = _tokenIds.current();
+                _mint(owner, id);
+                registry[id] = name;
+                registered[name] = true;
+
+                _tokenIds.increment();
+                return id;
+        }
+
+        function map(uint256 tokenId, address to) public {
+                require(msg.sender == ownerOf(tokenId));
+                resolver[tokenId] = to;
+        }
+}
