61c0f2e4c726cc94beae0aa29d41fdd4f56b7949
bld4666 <bld4666@example.com>
2023-05-05 14:48:44 +0700

    update multisend script

--- scripts/multi.task.ts
+++ scripts/multi.task.ts
@@ -2,21 +2,24 @@ import { task, types } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 task("multisend", "multisend")
-	.addOptionalParam("count", "number of receivers", 5, types.int)
-	.addParam("amount", "amount")
+	.addParam("contract", "contract address", "0x1899b9457823Fd7E24C9Ffc340D140c8b5477b7f", types.string)
 	.setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
 		const { deployments, ethers } = hre;
 		const accs = await ethers.getSigners();
 
 		const fac = await ethers.getContractFactory("Multisend");
-		const d = await deployments.get("Multisend");
-		const c = fac.attach(d.address).connect(accs[0]); // account 0 must be funded
-		const addresses = accs.slice(1, 1 + taskArgs.count).map(a => a.address)
-		const amounts = [];
+		let contractAddress = taskArgs.contract;
+		// const d = await deployments.get("Multisend");
+		const c = fac.attach(contractAddress).connect(accs[0]); // account 0 must be funded
+		const addresses = ['', '']; // your addresses
+		const amounts = [ethers.utils.parseEther('0.01'), ethers.utils.parseEther('0.02')]; // your amounts
+
+		if (addresses.length != amounts.length) {
+			throw new Error("addresses and amounts must be the same length");
+		}
 		let sumAmount = ethers.BigNumber.from(0);
-		for (let i = 0; i < taskArgs.count; i++) {
-			amounts.push(ethers.utils.parseEther(taskArgs.amount));
-			sumAmount = sumAmount.add(ethers.utils.parseEther(taskArgs.amount));
+		for (let i = 0; i < amounts.length; i++) {
+			sumAmount = sumAmount.add(amounts[i]);
 		}
 		console.log("Addresses to send to:", addresses);
 		console.log("Amounts to send:", amounts);
