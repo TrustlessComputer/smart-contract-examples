7fee701208a28bab6fb193f7acde2097e2e0eb91
bld4666 <bld4666@example.com>
2023-05-05 14:25:46 +0700

    update multisend script

--- scripts/multi.task.ts
+++ scripts/multi.task.ts
@@ -2,8 +2,6 @@ import { task, types } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 task("multisend", "multisend")
-	.addOptionalParam("count", "number of receivers", 5, types.int)
-	.addParam("amount", "amount")
 	.setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
 		const { deployments, ethers } = hre;
 		const accs = await ethers.getSigners();
@@ -11,12 +9,11 @@ task("multisend", "multisend")
 		const fac = await ethers.getContractFactory("Multisend");
 		const d = await deployments.get("Multisend");
 		const c = fac.attach(d.address).connect(accs[0]); // account 0 must be funded
-		const addresses = accs.slice(1, 1 + taskArgs.count).map(a => a.address)
-		const amounts = [];
+		const addresses = ['', '']; // your addresses
+		const amounts = [ethers.utils.parseEther('0.01'), ethers.utils.parseEther('0.02')];
 		let sumAmount = ethers.BigNumber.from(0);
-		for (let i = 0; i < taskArgs.count; i++) {
-			amounts.push(ethers.utils.parseEther(taskArgs.amount));
-			sumAmount = sumAmount.add(ethers.utils.parseEther(taskArgs.amount));
+		for (let i = 0; i < amounts.length; i++) {
+			sumAmount = sumAmount.add(amounts[i]);
 		}
 		console.log("Addresses to send to:", addresses);
 		console.log("Amounts to send:", amounts);
